d25935dfc73b81ba80ad5776a01f17b85c2b8523
Nathan Nunes <naythann88@gmail.com>
2022-03-27 12:59:25 -0300

    implemented both strategies

--- src/attacker.c
+++ src/attacker.c
@@ -1,5 +1,8 @@
 // Standard headers
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
 // Internal headers
 #include "direction.h"
@@ -16,14 +19,237 @@
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
+static position_t get_position_from_string(char string[100]){
+    
+    char delim[2];
+    delim[0] = ' ';
+    delim[1] = 0;
+    position_t result;
+
+    char* token = strtok(string, delim);
+
+    result.i = atoi(token);
+    token = strtok(0, delim);
+    result.j = atoi(token);
+
+    return result;
+}
+
+static direction_t get_direction_from_string(char string[100]){
+    
+    char delim[2];
+    delim[0] = ' ';
+    delim[1] = 0;
+    direction_t result;
+
+    char* token = strtok(string, delim);
+
+    result.i = atoi(token);
+    token = strtok(0, delim);
+    result.j = atoi(token);
+
+    return result;
+}
+
+static int get_last_line_turno() {
+	FILE *fd;                           // File pointer
+	char filename[] = "round_counter_attacker.txt";       // file to read
+	static const long max_len = 5;  // define the max length of the line to read
+	char buff[max_len + 1];             // define the buffer and allocate the length
+
+	if ((fd = fopen(filename, "r")) != NULL)  {      // open file. I omit error checks
+
+		while( fgets(buff, sizeof(buff), fd)!=NULL );
+		fclose(fd);
+    	return atoi(buff);
+
+	}
+	return 0;
+}
+
+static int get_last_turno(int turno, position_t current_position, position_t last_position){
+    if (abs((int)current_position.i - (int)last_position.i) > 2 || abs((int)current_position.j - (int)last_position.j) > 2){
+        return 0;
+    }else{
+        return turno;
+    }
+}
+
+position_t get_last_attacker_position(){
+    FILE *fd;                           // File pointer
+	char filename[] = "attacker_last_position.txt";       // file to read
+	static const long max_len = 100;  // define the max length of the line to read
+	char buff[max_len + 1];             // define the buffer and allocate the length
+
+	if ((fd = fopen(filename, "r")) != NULL)  {      // open file. I omit error checks
+
+		while( fgets(buff, sizeof(buff), fd)!=NULL );
+		fclose(fd);
+    	return get_position_from_string(buff);
+	}
+	return (position_t) {0, 0};
+}
+
+static void save_defender_position(position_t defender_position){
+    FILE *fd;                           // File pointer
+	char filename[] = "last_defender_position_spy.txt";       // file to read
+    fd = fopen(filename, "a+");
+    fprintf(fd, "%ld %ld\n", defender_position.i, defender_position.j);
+    fclose(fd);
+}
+
+
+position_t get_defender_position(){
+    FILE *fd;                           // File pointer
+	char filename[] = "last_defender_position_spy.txt";       // file to read
+	static const long max_len = 100;  // define the max length of the line to read
+	char buff[max_len + 1];             // define the buffer and allocate the length
+
+	if ((fd = fopen(filename, "r")) != NULL)  {      // open file. I omit error checks
+
+		while( fgets(buff, sizeof(buff), fd)!=NULL );
+		fclose(fd);
+    	return get_position_from_string(buff);
+
+	}
+	return (position_t) {0, 0};
+}
+
+
+static void save_turno(int turno){
+    FILE *fd;                           // File pointer
+	char filename[] = "round_counter_attacker.txt";       // file to read
+    fd = fopen(filename, "a+");
+    fprintf(fd, "%d\n", turno);
+    fclose(fd);
+
+}
+
+static direction_t get_last_move(int made_move){
+    if(!made_move)
+        return (direction_t) {0, 0};
+    FILE *fd;                           // File pointer
+	char filename[] = "last_attacker_move.txt";       // file to read
+	static const long max_len = 100;  // define the max length of the line to read
+	char buff[max_len + 1];             // define the buffer and allocate the length
+
+	if ((fd = fopen(filename, "r")) != NULL)  {      // open file. I omit error checks
+
+		while( fgets(buff, sizeof(buff), fd)!=NULL );
+		fclose(fd);
+    	return get_direction_from_string(buff);
+
+	}
+	return (direction_t) {0, 0};
+}
+
+void save_last_move(direction_t move){
+    FILE *fd;                           // File pointer
+	char filename[] = "last_attacker_move.txt";       // file to read
+    fd = fopen(filename, "a+");
+    fprintf(fd, "%d %d\n", move.i, move.j);
+    fclose(fd);
+}
+
+static int made_move_last_turn(position_t current_position){
+    position_t last_position = get_last_attacker_position();
+    if (current_position.i != last_position.i || current_position.j != last_position.j){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+static void save_attacker_position(position_t attacker_position){
+    FILE *fd;                           // File pointer
+	char filename[] = "attacker_last_position.txt";       // file to read
+    fd = fopen(filename, "a+");
+    fprintf(fd, "%ld %ld\n", attacker_position.i, attacker_position.j);
+    fclose(fd);
+}
+
+int compare_direction(direction_t a, direction_t b){
+    if (a.i == b.i && a.j == b.j){
+        return 1;
+    }
+    return 0;
+}
+
+direction_t decide_direction_attacker(int turno, int made_move, position_t attacker_position, position_t defender_position, direction_t last_move){
+    //printf("Decisão: turno:%d, made_move:%d, last_move.i:%d, last_move.j:%d, diferença.i:%d\n", turno, made_move, last_move.i, last_move.j, abs((int)attacker_position.i - (int)defender_position.i));
+    if (turno < 7){
+        return (direction_t) {0, 0};
+    }
+    else if (turno < 12){
+        if (made_move && compare_direction(last_move, (direction_t) DIR_UP)){
+            return (direction_t) DIR_UP;
+        }else if (made_move && compare_direction(last_move, (direction_t) DIR_RIGHT)) {
+            return (direction_t) DIR_RIGHT;
+        }else if (!made_move && compare_direction(last_move, (direction_t) DIR_UP)){
+            return (direction_t) DIR_RIGHT;
+        }else if (!made_move && compare_direction(last_move, (direction_t) DIR_RIGHT)){
+            return (direction_t) DIR_UP;
+        } else {
+            return (direction_t) DIR_UP;
+        }
+    }
+    else if (turno >= 12){
+        if (abs((int)attacker_position.i - (int)defender_position.i) <= 3){
+            if ((made_move && compare_direction(last_move, (direction_t) DIR_DOWN)) || turno == 12){
+                return (direction_t) DIR_DOWN;
+            }else if (made_move && compare_direction(last_move, (direction_t) DIR_RIGHT)) {
+                return (direction_t) DIR_RIGHT;
+            }else if (!made_move && compare_direction(last_move, (direction_t) DIR_DOWN)){
+                return (direction_t) DIR_RIGHT;
+            }else if (!made_move && compare_direction(last_move, (direction_t) DIR_RIGHT)){
+                return (direction_t) DIR_DOWN;
+            } else {
+                return (direction_t) DIR_DOWN;
+            }
+        } else {
+            if (made_move && compare_direction(last_move, (direction_t) DIR_UP)){
+                return (direction_t) DIR_UP;
+            }else if (made_move && compare_direction(last_move, (direction_t) DIR_RIGHT)) {
+                return (direction_t) DIR_RIGHT;
+            }else if (!made_move && compare_direction(last_move, (direction_t) DIR_UP)){
+                return (direction_t) DIR_RIGHT;
+            }else if (!made_move && compare_direction(last_move, (direction_t) DIR_RIGHT)){
+                return (direction_t) DIR_UP;
+            } else {
+                return (direction_t) DIR_RIGHT;
+            }
+        }
+    }
+    return (direction_t) {0, 0};
+}
+
+
 direction_t execute_attacker_strategy(
     position_t attacker_position, Spy defender_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(attacker_position);
-  UNUSED(defender_spy);
+    int turno = get_last_line_turno();
+    position_t last_position = get_last_attacker_position(attacker_position);
+    turno = get_last_turno(turno, attacker_position, last_position);
+    int made_move = made_move_last_turn(attacker_position);
+    direction_t movement_direction;
+    direction_t last_move = get_last_move(made_move);
+    position_t defender_position;
+    turno++;
+    
+    if (turno == 12){
+        defender_position = get_spy_position(defender_spy);
+        save_defender_position(defender_position);
+    } else if (turno > 12){
+        defender_position = get_defender_position();
+    } else {
+        defender_position = (position_t) {0, 0};
+    }
+    
+    movement_direction = decide_direction_attacker(turno, made_move, attacker_position, defender_position, last_move);
+    save_last_move(movement_direction);
+    save_attacker_position(attacker_position);
+    save_turno(turno);
 
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+    return movement_direction;
 }
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -1,5 +1,8 @@
 // Standard headers
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
 // Internal headers
 #include "direction.h"
@@ -16,14 +19,159 @@
 /*                              PUBLIC FUNCTIONS                              */
 /*----------------------------------------------------------------------------*/
 
+position_t get_position_from_string(char string[100]){
+    
+    char delim[2];
+    delim[0] = ' ';
+    delim[1] = 0;
+    position_t result;
+
+    char* token = strtok(string, delim);
+
+    result.i = atoi(token);
+    token = strtok(0, delim);
+    result.j = atoi(token);
+
+    return result;
+}
+
+int get_last_line_turno() {
+	FILE *fd;                           // File pointer
+	char filename[] = "round_counter_defender.txt";       // file to read
+	static const long max_len = 5;  // define the max length of the line to read
+	char buff[max_len + 1];             // define the buffer and allocate the length
+
+	if ((fd = fopen(filename, "r")) != NULL)  {      // open file. I omit error checks
+
+		while( fgets(buff, sizeof(buff), fd)!=NULL );
+		fclose(fd);
+    	return atoi(buff);
+
+	}
+	return 0;
+}
+
+int get_last_turno(int turno, position_t current_position, position_t last_position){
+    if (abs((int)current_position.i - (int)last_position.i) > 2 || abs((int)current_position.j - (int)last_position.j) > 2){
+        return 0;
+    }else{
+        return turno;
+    }
+}
+
+position_t get_last_defender_position(){
+    FILE *fd;                           // File pointer
+	char filename[] = "defender_last_position.txt";       // file to read
+	static const long max_len = 100;  // define the max length of the line to read
+	char buff[max_len + 1];             // define the buffer and allocate the length
+
+	if ((fd = fopen(filename, "r")) != NULL)  {      // open file. I omit error checks
+
+		while( fgets(buff, sizeof(buff), fd)!=NULL );
+		fclose(fd);
+    	return get_position_from_string(buff);
+
+	}
+	return (position_t) {0, 0};
+}
+
+void save_attacker_position(position_t attacker_position){
+    FILE *fd;                           // File pointer
+	char filename[] = "last_attacker_position_spy.txt";       // file to read
+    fd = fopen(filename, "a+");
+    fprintf(fd, "%ld %ld\n", attacker_position.i, attacker_position.j);
+    fclose(fd);
+}
+
+
+position_t get_attacker_position(){
+    FILE *fd;                           // File pointer
+	char filename[] = "last_attacker_position_spy.txt";       // file to read
+	static const long max_len = 100;  // define the max length of the line to read
+	char buff[max_len + 1];             // define the buffer and allocate the length
+
+	if ((fd = fopen(filename, "r")) != NULL)  {      // open file. I omit error checks
+
+		while( fgets(buff, sizeof(buff), fd)!=NULL );
+		fclose(fd);
+    	return get_position_from_string(buff);
+
+	}
+	return (position_t) {0, 0};
+}
+
+
+void save_turno(int turno){
+    FILE *fd;                           // File pointer
+	char filename[] = "round_counter_defender.txt";       // file to read
+    fd = fopen(filename, "a+");
+    fprintf(fd, "%d\n", turno);
+    fclose(fd);
+
+}
+
+int made_move_last_turn(position_t current_position){
+    position_t last_position = get_last_defender_position();
+    if (current_position.i != last_position.i || current_position.j != last_position.j){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void save_defender_position(position_t defender_position){
+    FILE *fd;                           // File pointer
+	char filename[] = "defender_last_position.txt";       // file to read
+    fd = fopen(filename, "a+");
+    fprintf(fd, "%ld %ld\n", defender_position.i, defender_position.j);
+    fclose(fd);
+}
+
+direction_t decide_direction_defender(int turno, int made_move, position_t attacker_position, position_t defender_position){
+    if (turno < 8){
+        return (direction_t) {0, 0};
+    }
+    else if (turno >= 8){
+        if (made_move || turno == 8){
+            if (attacker_position.i > defender_position.i){
+                return (direction_t) DIR_DOWN;
+            }else {
+                return (direction_t) DIR_UP;
+            }
+        }else {
+            return (direction_t) {0, 0};
+        }
+    }
+    return (direction_t) {0, 0};
+}
+
+
 direction_t execute_defender_strategy(
     position_t defender_position, Spy attacker_spy) {
-  // TODO: unused parameters, remove these lines later
-  UNUSED(defender_position);
-  UNUSED(attacker_spy);
+  	
+	int turno = get_last_line_turno();
+    position_t last_position = get_last_defender_position(defender_position);
+    turno = get_last_turno(turno, defender_position, last_position);
+    
+    direction_t movement_direction;
+    position_t attacker_position;
+    turno++;
+    
+    if (turno == 8){
+        attacker_position = get_spy_position(attacker_spy);
+        save_attacker_position(attacker_position);
+    } else if (turno > 8){
+        attacker_position = get_attacker_position();
+    } else {
+        attacker_position = (position_t) {0, 0};
+    }
+    int made_move = made_move_last_turn(defender_position);
+    movement_direction = decide_direction_defender(turno, made_move, attacker_position, defender_position);
 
-  // TODO: Implement Defender logic here
-  return (direction_t) DIR_LEFT;
+    save_defender_position(defender_position);
+    save_turno(turno);
+
+    return movement_direction;
 }
 
 /*----------------------------------------------------------------------------*/
